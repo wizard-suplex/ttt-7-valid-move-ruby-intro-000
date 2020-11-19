4c3cbe83c5758f12dc6791cd343eeb2ed9461352
Alan Cooke <atc4@rice.edu>
2020-11-19 20:58:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,9 +2,10 @@
 def valid_move?(board, index)
   if index.between?(0,8) == true
     return true
-  elsif board[index] == "X" || board[index] == "O"
-    return false
+  elsif board[index] !== "X" || board[index] !== "O"
+    return true
   else
+    return false
   end
 end
 
