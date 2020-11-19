f5be7ee367468f4a63d1a3ec709ebd7f0b76fba3
Alan Cooke <atc4@rice.edu>
2020-11-19 20:54:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if board[index].between(0,8)
+  if board[index].between(0,8) == true
     return true
+  elsif board[index] == "X" || board[index] == "O"
+    return false
+    
   else
   end
 end
