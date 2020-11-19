14a6047a8e574f6a4f01510d872d99bd6bbb776f
Alan Cooke <atc4@rice.edu>
2020-11-19 21:01:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(0,8) == true
+  if index.between?(0,8) == true && board[index] == " "
     return true
   elsif board[index] == " "
     return true
